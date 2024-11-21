b63ca16a6191741ae2a146e931aa29224b70df9a
thesagor <nilsagor004@gmail.com>
2024-11-21 07:55:43 +0600

    js has been started

---
+++ index.js
@@ -0,0 +1,77 @@
+// const students = [
+//   {
+//     name: "John",
+//     age: 20,
+//     name: "Johnson",
+//     age: 30,
+//     name: "Jane",
+//     age: 25,
+//     name: "Jane",
+//     age: 28,
+//     name: "Jack",
+//     age: 22,
+//     name: "Jackson",
+//     age: 27,
+//     name: "Jill",
+//     age: 23,
+//     name: "Jill",
+//     age: 26,
+//     // Add more students here
+//   },
+// ];
+
+// // Function to filter students by name and age
+// let mystudent = students.filter((student) => students.age < 25);
+// console.log(mystudent);
+
+// const students = [
+//   { name: "John", age: 20 },
+//   { name: "Johnson", age: 30 },
+//   { name: "Jane", age: 25 },
+//   { name: "Jack", age: 22 },
+//   { name: "Jackson", age: 27 },
+//   { name: "Jill", age: 23 },
+//   // Add more students here
+// ];
+
+// // Function to filter students by age
+// let myStudents = students.filter((student) => student.age < 25);
+// console.log(myStudents);
+
+//set timeout(() =>
+
+// setTimeout(() => {
+//   console.log("Hello boss!");
+// }, 1000);
+
+//setinterval(() =>
+let count = 0;
+
+// let timer = setInterval(() => {
+//   console.log("I'm always here!");
+//   count++;
+//   if (count === 5) {
+//     clearInterval(timer);
+//   }
+// }, 5000);
+// // clearInterval(timer);
+
+//deposit and withdraw
+
+//create a object for my pc
+
+//create a object for my laptop
+const laptop = {
+  ram: "8gb",
+  storage: "1tb",
+  processor: "i7",
+  monitor: "15inch",
+  keyboard: "backlit",
+  mouse: "wireless",
+  touchpad: "yes",
+  display: "1920x1080",
+  battery: "12000mAh",
+  weight: "1.3kg",
+};
+
+console.log(laptop);
