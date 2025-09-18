756c808e93e21496b51536ade4ef2848ec448dec
thesagor <nilsagor004@gmail.com>
2025-09-18 08:00:14 +0600

    has been changed

---
+++ myFile.js
@@ -0,0 +1,9 @@
+console.log("hello i am in myFile.js")
+
+let goto = "google.com"
+let file = "myFile.js"
+let final = "this is another"
+let chars = 'abcdefghijklmnopqrstuvwxyz'
+let template = `this is a template string with ${goto} and ${file} and ${final}`
+
+console.log(template)
