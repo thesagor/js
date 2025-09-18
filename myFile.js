7c97b2e636098b16150ee49c6574b1c7a5bed285
thesagor <nilsagor004@gmail.com>
2025-09-18 08:17:30 +0600

    has been changed

--- myFile.js
+++ myFile.js
@@ -1,9 +1,14 @@
-//arithmatic operations
+/*//arithmatic operations
     let a = 10;
     let b = 3;
     console.log("THis is add "+ (a+b));
     console.log("THis is subtition "+ (a-b));
     console.log("THis is multiplition "+ (a*b));
     console.log("THis is division "+ (a/b));
-    console.log("THis is modulas "+ (a%b));
+    console.log("THis is modulas "+ (a%b));*/
 
+
+//assignment operator
+let c=10
+console.log(c+=10)
+console.log(c*=c)
