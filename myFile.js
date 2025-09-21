2ccd4de2859279725fc40b41e518d878bac80f40
thesagor <nilsagor004@gmail.com>
2025-09-21 14:00:16 +0600

    many type of function has been learned

--- myFile.js
+++ myFile.js
@@ -76,8 +76,8 @@ console.log(vatCalculator(500));*/
 
 
 //bill and vat calculation using switch case
-let bill ;
-function vatCalculator(bill){
+// let bill ;
+/*function vatCalculator(bill){
     switch(true){
         case(bill>=1500):
             return bill*.20;
@@ -85,11 +85,95 @@ function vatCalculator(bill){
         case(bill>=500 && bill<=1500):
             return bill*.15;
             break;
-            case(bill<=500):
+            case(bill<=499):
             return "no need to pay vat";
             break;
         default:
             return "invalid bill amount";
     }
 }
-console.log(vatCalculator(1000));
+console.log(vatCalculator(100));*/
+
+/*let bill ;
+function vatCalculator(bill){
+    if(bill>=1500){
+        return bill*.20;
+    }else{
+        return "no need to pay vat";
+    }
+}
+console.log(vatCalculator(15000));*/
+
+
+//age calculator
+/*
+const sagor=25;
+const rafi =25;
+switch(true) {
+    case (sagor > rafi):
+        console.log("sagor is older");
+        break;
+    case (sagor < rafi):
+        console.log("rafi is older");
+        break;
+    default:
+        console.log("both are same age");
+
+
+}*/
+
+/*const day=12;
+switch(day){
+    case 0:
+        console.log("sunday");
+        break;
+    case 1:
+        console.log("monday");
+        break;
+    case 2:
+        console.log("tuesday");
+        break;
+    case 3:
+        console.log("wednesday");
+        break;
+    case 4:
+        console.log("thursday");
+        break;
+    case 5:
+        console.log("friday");
+        break;
+    case 6:
+        console.log("saturday");
+        break;
+    default:
+        console.log("invalid day");
+
+}*/
+
+
+// //ternary operator
+//  let age=20;
+//  let checkBiyerAge= age>=20 ? "you can biye right now":"hey na tomr boyos hoyni"
+//  console.log(checkBiyerAge)
+
+//loop  
+
+/*let num=1
+while(num<10){
+    console.log(num)
+    num++
+
+}*/
+
+/*
+//do while loop
+let name = 'Sagoe Ahmed';
+while(true){
+    console.log(name);
+}
+*/
+
+
+
+
+
